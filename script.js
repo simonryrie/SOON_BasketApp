361c940799d4c7383bac36700c13e8266427fae5
simonryrie <simonryrie@gmail.com>
2023-03-16 16:45:47 +0000

    Cookies functionality added

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+
+function deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+deleteAllCookies()
+
+
 let item1234 = {
     name : "Waxed Cotton Hooded Jacket",
     description : "The Drumming jacket in orange is finished with a water-repellent dry wax treatment that creates a love-worn look. It's made in the United Kingdom using organic cotton ripstop with a drawstring hood, underarm eyelets and buttoned flap front pockets. Shoulder epaulettes add a utilitarian twist, while a fly-fronted zip and snap-button closure keeps the overall look streamlined. Attach one of the collection's padded liners to the internal tab on cooler days.",
@@ -11,6 +27,7 @@ let item1234 = {
 window.onload = () => {
     updatePage(item1234);
     addEventListeners();
+    displayBasketTotal();
 };
 
 // declare basket (local storage functionality added later)
@@ -56,6 +73,19 @@ function updatePage(item) {
     document.getElementById("numberInStock").textContent = stock;
 };
 
+// function to ensure basket total is displayed from cookies on load
+
+function displayBasketTotal() {
+    let basket = JSON.parse(getCookie("basket"));
+    if (!basket) {
+        basket = {};
+    }
+    const arrayOfItemNumbers = Object.values(basket);
+    const basketTotal = arrayOfItemNumbers.reduce((sum, current) => sum + current, 0);
+    const basketCounter = document.getElementById("amountInBasket");
+    basketCounter.textContent = basketTotal;
+}
+
 // function to increment the number of items to add
 
 function changeNumberToAdd(button, stock) {
@@ -79,6 +109,11 @@ function addToBasket() {
     let numberToAdd = parseInt(document.getElementById("numberToAdd").textContent);
     const numberInStock = item1234.stock;
     
+    let basket = JSON.parse(getCookie("basket"));
+    if (!basket) {
+        basket = {};
+    }
+    
     if (basket[itemName]) {
         const totalNumber = basket[itemName] + numberToAdd;
         if (totalNumber > numberInStock) {
@@ -92,13 +127,18 @@ function addToBasket() {
         basket[itemName] = numberToAdd; 
     }
     
+    setCookie("basket", JSON.stringify(basket), 365);
     document.getElementById("numberToAdd").textContent = 1;
     updateBasketTotal();
 };
 
-// function to update total items in basket
+// function to update the basket total
 
 function updateBasketTotal() {
+    let basket = JSON.parse(getCookie("basket"));
+    if (!basket) {
+        basket = {};
+    }
     const arrayOfItemNumbers = Object.values(basket);
     const basketTotal = arrayOfItemNumbers.reduce((sum, current) => sum + current, 0);
     const basketCounter = document.getElementById("amountInBasket");
@@ -110,6 +150,25 @@ function updateBasketTotal() {
         animateBasket("fail");
     }
 };
+
+// cookies set and get functions
+
+function setCookie(name, value, days) {
+    const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
+    document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split("=");
+        if (cookieName === name) {
+            return cookieValue;
+        }
+    }
+    return null;
+}
+
 
 // function to highlight basket with colour change on submission - red if no more items can be added
 
